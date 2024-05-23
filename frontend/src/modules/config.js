1ddef4fcd5fd186b56b0240019b99ad297175ca0
Alexander Neumann <aleneum@gmail.com>
2024-05-23 16:33:38 +0200

    avoid overlaps with dagre

--- frontend/src/modules/config.js
+++ frontend/src/modules/config.js
@@ -1,7 +1,10 @@
 var config = {
   layouts: {
     concentric: { name: 'concentric', minNodeSpacing: 100 },
-    dagre: { name: 'dagre', ranker: 'tight-tree' },
+    dagre: {
+      name: 'dagre', ranker: 'tight-tree', avoidOverlap: true,
+      nodeDimensionsIncludeLabels: true
+    },
     bilkent: { name: 'cose-bilkent', idealEdgeLength: 100 }
   },
   machineStorageLocation: 'transitions.layouts.machines'
